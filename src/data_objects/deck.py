cda2ec941a62de21ff737697a4acf586adfd6171
CLopMan <cesarlopman@gmail.com>
2025-11-28 18:20:50 +0100

    tag filtering

--- src/data_objects/deck.py
+++ src/data_objects/deck.py
@@ -1,58 +1,4 @@
-from dataclasses import dataclass
 from data_objects.question import Question
-
-@dataclass
-class OptionAnswer:
-    answer: str
-    value: bool
-
-    
-
-#class TrueFalseQuestion(Question):
-#    answers: list[OptionAnswer]
-#
-#    def __init__(self, question, tags, answers):
-#        super().__init__(question, tags)
-#        self.answers: list[OptionAnswer] = [OptionAnswer(**answer) for answer in answers]
-#        self.answer_num: int =  len(answers)
-#
-#    def evaluate(self, answer: list[bool]) -> bool:
-#        for i, x in enumerate(answer):
-#            if x != self.answers[i].value:
-#                return False
-#        return True
-#
-#    def to_json(self) -> str:
-#        return \
-#f"{{\
-#    \"type\": \"normal\",\
-#    \"tags\": {str(self.tags)},\
-#    \"answerNum\": 1,\
-#}}"
-#
-#    def __str__(self):
-#        return f"{self.question.upper()}:\n\t- {"\n\t- ".join([f"{a.answer} : {a.value}" for a in self.answers])}\n\ttags: {", ".join(self.tags)}"
-
-#class MultiSelectQuestion(TrueFalseQuestion):
-#
-#    def __init__(self, question, tags, answers, num = 0):
-#        super().__init__(question, tags, answers)
-#        self.answer_num: int = num if num > 0 else len(answers)
-#
-#    def evaluate(self, answer: list[int]) -> bool:
-#        out = len(answer) > 0
-#        for i in answer:
-#            out = out and self.answers[i].value
-#        return out
-#
-#    def to_json(self) -> str:
-#        return \
-#f"{{\
-#\"type\": \"normal\",\
-#\"tags\": {str(self.tags)},\
-#\"answerNum\": 1,\
-#}}"
-
  
 class Deck:
     title: str
@@ -63,6 +9,22 @@ class Deck:
         self.title = deck_name
         self.questions = questions
         self.card_num = len(questions)
+
+        possible_tags: set[str] = set()
+        for q in questions:
+            possible_tags = possible_tags.union( set(q.tags))
+
+        self.possible_tags: set[str] = possible_tags
+
+    def filter(self, tags: list[str]):
+        if len(tags) <= 0:
+            return self
+        tags_set = set(tags)
+        return Deck(
+                deck_name=self.title,
+                questions=[q for q in self.questions if len(tags_set.intersection(set(q.tags))) > 0]
+            )
+        
 
     def __len__(self) -> int:
         return self.card_num
